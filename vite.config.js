b416d694c4a183f1e5dca95f0320d058229b0e50
Artem Lobanov <artem.lobanov.w@gmail.com>
2024-12-25 00:52:06 +0300

    GP deploy2

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,13 @@
 export default {
-  base: '/test-image-webgl/', // имя репозитория
+  base: './', // Используем относительные пути
+  build: {
+    assetsDir: 'assets', // Директория для ассетов (по умолчанию "assets")
+  },
   esbuild: {
-    include: /src\/(.*\.js|.*\.jsx|.*\.ts|.*\.tsx)$/
+    include: /src\/(.*\.js|.*\.jsx|.*\.ts|.*\.tsx)$/,
   },
   server: {
     port: 3000,
     host: '0.0.0.0',
   },
-}
+};
